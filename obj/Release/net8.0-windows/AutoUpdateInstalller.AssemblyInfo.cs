f50d1810124c987253b4ccea9b5bcd9f233dcf43
Vanly KIM <vanlykim@gmail.com>
2024-09-27 05:16:02 -0500

    Update install and update from lacation 2

--- obj/Release/net8.0-windows/AutoUpdateInstalller.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/AutoUpdateInstalller.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoUpdateInstalller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd0ac68cd50420c016a22306be980fc98328c174")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69622693defb952720bcfcf5f9bf6636fbab9b7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoUpdateInstalller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoUpdateInstalller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
