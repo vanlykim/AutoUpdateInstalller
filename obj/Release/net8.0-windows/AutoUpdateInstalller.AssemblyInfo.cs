fd0ac68cd50420c016a22306be980fc98328c174
Vanly KIM <vanlykim@gmail.com>
2024-09-27 04:29:10 -0500

    Initial publish

--- obj/Release/net8.0-windows/AutoUpdateInstalller.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/AutoUpdateInstalller.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoUpdateInstalller")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+490608b0be8f0d3bc9ac22ae60b1f6476a456652")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoUpdateInstalller")]
